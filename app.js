0dc632e27679495079396dc8bdd9c636cb7b46e8
Ralph HONG <dev.inpyo@gmail.com>
2023-01-24 16:55:55 +0900

    CH_03_04 - DOM API를 제거하여 가시성을 늘림

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
-// ajax 호출
-ajax.open('get', NEWS_URL, false);
-ajax.send();
+
+const newsFeed = getData(NEWS_URL);
+
+function getData (url) {
+  // ajax 호출
+  ajax.open('get', url, false);
+  ajax.send();
 
 // ajax 리스폰스
-let newsFeed = JSON.parse(ajax.response);
-
-console.log(newsFeed);
-/*데이터 가져오기 완료*/
+  return JSON.parse(ajax.response);
+}
 
 
 /*데이터를 표현할 태그 생성*/
@@ -25,10 +27,7 @@ const ul = document.createElement('ul');
 window.addEventListener('hashchange', () => {
 
   const id = location.hash.substring(1);
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
   const title = document.createElement('h1');
 
   title.innerHTML = newsContent.title
@@ -38,15 +37,18 @@ window.addEventListener('hashchange', () => {
 /*데이터를 생성된 태그 안에 넣음*/
 // li태그 갯수대로 반복
 for (let i = 0; i < newsFeed.length; i++) {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
+  const div = document.createElement('div');
 
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-  a.addEventListener('click', () => {})
+  /*DOM API를 사용하면 구조를 명확하게 파악할 수 없어서 innerHTML과 문자열 템플릿을 이용하여 가시성이 좋게 만듬.*/
+  div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">
+      ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+    `;
 
-  li.appendChild(a);
-  ul.appendChild(li); // ul태그 밑에 li를 붙임
+  ul.appendChild(div.firstElementChild);
 }
 
 
